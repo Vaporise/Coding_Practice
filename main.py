ee622a78d70943ea9f0aec40e9b39c1ab2c5bf3f
Vaporise <draxdragonfire@googlemail.com>
2025-04-30 22:02:00 +0900

    added comments to my code

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import turtle
 
-screen = turtle.Screen()
-screen.setup(1.0, 1.0)
+screen = turtle.Screen() #creates a screen using the turtle library
+screen.setup(1.0, 1.0) #setups up the screen to size 1 and position 1
 
-t = turtle.Turtle()
+t = turtle.Turtle() 
 
-for c in ["red", "green", "blue", "yellow"]:
-    t.color(c)
-    t.forward(75)
-    t.left(90)
+for c in ["red", "green", "blue", "yellow"]: #iterates through 4 colours where c is red/green/blue/yellow
+    t.color(c) # set color to c
+    t.forward(75) # move forward 75
+    t.left(90) # turn left 90 degrees
 
-    screen.mainloop()
+screen.mainloop() #took out of for loop to fix issue.
