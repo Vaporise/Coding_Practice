6182823b1b9ed89ea065cfbed0bef8b0e062d523
Vaporise <draxdragonfire@googlemail.com>
2025-04-30 21:58:28 +0900

    Tried to make a square with turtle library but only one colour worked

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import turtle
 
-print("Hello World")
+screen = turtle.Screen()
+screen.setup(1.0, 1.0)
+
+t = turtle.Turtle()
+
+for c in ["red", "green", "blue", "yellow"]:
+    t.color(c)
+    t.forward(75)
+    t.left(90)
+
+    screen.mainloop()
