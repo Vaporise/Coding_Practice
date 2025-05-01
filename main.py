2db17be9fbb397bf2dd2d4eca95a1a75cb5e9bc4
Vaporise <draxdragonfire@googlemail.com>
2025-05-01 09:16:09 +0900

    tested the forward function

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import turtle
+from turtle import *
 
-screen = turtle.Screen() #creates a screen using the turtle library
-screen.setup(1.0, 1.0) #setups up the screen to size 1 and position 1
+forward(100)
 
-t = turtle.Turtle() 
+done()
 
-for c in ["red", "green", "blue", "yellow"]: #iterates through 4 colours where c is red/green/blue/yellow
-    t.color(c) # set color to c
-    t.forward(75) # move forward 75
-    t.left(90) # turn left 90 degrees
-
-screen.mainloop() #took out of for loop to fix issue.
